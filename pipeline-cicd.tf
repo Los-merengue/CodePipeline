a6db33a8a1d1d3c2e5808a26cc7e0e9aa6fc5a3c
los Merengue <michaels4uandme@gmail.com>
2022-02-03 15:53:52 +0100

    created the pipeline

--- pipeline-cicd.tf
+++ pipeline-cicd.tf
@@ -47,3 +47,63 @@ resource "aws_codebuild_project" "tf-apply" {
      buildspec = file("buildspec/apply-buildspec.yml")
  }
 }
+
+resource "aws_codepipeline" "cicd_pipeline" {
+
+    name = "tf-cicd"
+    role_arn = aws_iam_role.tf-codepipeline-role.arn
+
+    artifact_store {
+        type="S3"
+        location = aws_s3_bucket.codepipeline_artifacts.id
+    }
+
+    stage {
+        name = "Source"
+        action{
+            name = "Source"
+            category = "Source"
+            owner = "AWS"
+            provider = "CodeStarSourceConnection"
+            version = "1"
+            output_artifacts = ["tf-code"]
+            configuration = {
+                FullRepositoryId = "davoclock/aws-cicd-pipeline"
+                BranchName   = "master"
+                ConnectionArn = var.codestar_connector_credentials
+                OutputArtifactFormat = "CODE_ZIP"
+            }
+        }
+    }
+
+    stage {
+        name ="Plan"
+        action{
+            name = "Build"
+            category = "Build"
+            provider = "CodeBuild"
+            version = "1"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+            configuration = {
+                ProjectName = "tf-cicd-plan"
+            }
+        }
+    }
+
+    stage {
+        name ="Deploy"
+        action{
+            name = "Deploy"
+            category = "Build"
+            provider = "CodeBuild"
+            version = "1"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+            configuration = {
+                ProjectName = "tf-cicd-apply"
+            }
+        }
+    }
+
+}

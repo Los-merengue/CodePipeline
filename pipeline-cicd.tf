3e3ef4ec366137b0af116e83635482d07310058c
los Merengue <michaels4uandme@gmail.com>
2022-02-03 15:37:07 +0100

    added codebuild for tf-plan

---
+++ pipeline-cicd.tf
@@ -0,0 +1,24 @@
+resource "aws_codebuild_project" "tf-plan" {
+  name          = "tf-cicd-plan2"
+  description   = "Plan stage for terraform"
+  service_role  = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = ##Container for dockerhub
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    registry_credential{
+        credential = var.dockerhub_credentials
+        credential_provider = "SECRETS_MANAGER"
+    }
+ }
+ source {
+     type   = "CODEPIPELINE"
+     buildspec = file("buildspec/plan-buildspec.yml")
+ }
+}

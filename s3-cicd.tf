1ccad394ebe6bae6ef9e6b8c03645bb8020b1b23
los Merengue <michaels4uandme@gmail.com>
2022-02-04 01:19:37 +0100

    added comment

--- s3-cicd.tf
+++ s3-cicd.tf
@@ -1,4 +1,5 @@
 # Provisioning an S3 Bucket
+# This s3 Bucket would carry the code artifacts that would be used
 
 resource "aws_s3_bucket" "codepipeline_artifacts" {
   bucket = "pipeline-artifacts-735353624204"

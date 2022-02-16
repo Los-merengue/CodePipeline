38d897cc95328b37fdef6008f31d7dd5ccd39d10
los Merengue <michaels4uandme@gmail.com>
2022-02-16 01:44:57 +0100

    modify the pipeline-cicd.tf

--- pipeline-cicd.tf
+++ pipeline-cicd.tf
@@ -11,7 +11,7 @@ resource "aws_codebuild_project" "tf-plan" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = ##Container from dockerhub
+    image                       = ##Container image from dockerhub
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
     registry_credential{
